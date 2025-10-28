bf864b87f6a9df0810b68d6d491b53bf1b32f995
RaddIsHere <gamingdocomo@gmail.com>
2025-10-28 19:07:34 +0700

    Add files via upload

---
+++ tugasp4/Latihan5.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugasp4;
+
+/**
+ *
+ * @author HYPE AMD
+ */
+public class Latihan5 {
+    
+}
